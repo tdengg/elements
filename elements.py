cae83a73d5a02252fa52c80668a4588306880732
Thomas Dengg <thomas.dengg@stud.unileoben.ac.at>
2010-10-24 01:09:00 +0200

    modifications of input setup finished

--- elements.py
+++ elements.py
@@ -1,45 +1,34 @@
 import sys
-elementshome = '/fshome/tde/git/my_calc/gen/elements/'      # define location of elements home here!
-sys.path.append(elementshome)
 import eos_calc_use_templ as calc
 import series
+import my_calcsetup
 
-param = {}
-usr = "tde"
-scale = []
-covera = []
-volume = []
+setup = my_calcsetup.element
+azero = setup['param']['scale']['azero']
+da = setup['param']['scale']['da']
+asteps = setup['param']['scale']['steps']
+coverazero = setup['param']['covera']['coverazero']
+dcovera = setup['param']['covera']['dcovera']
+coasteps = setup['param']['covera']['steps']
 
-azero = 4.319                                   #lattice parameter
-da = 0.05                                   #steps in lattice parameter
-coverazero = 1.6                                #c/a ratio
-dcovera = 1.6/50                                #steps in c/a
-param['structure'] = ['hcp']
-param['mod'] = ['eos']
+del setup['param']['scale']
+del setup['param']['covera']
 
-expand = series.Series(param['structure'])      #instance of series expansion class
+expand = series.Series(setup['structure'])      #instance of series expansion class
 
 scale = expand.latt_steps(azero, da, 11)    #generate 11 steps in lattice parameter
 
-if param['structure'][0] in ['hcp','hex'] and param['mod'][0] != 'simple_conv':
+if setup['structure'] in ['hcp','hex'] and setup['mod'] != 'simple_conv':
     vzero = azero**3 * coverazero * 3**(1/2.)/2 #initial volume 
     dvolume = vzero/50                          #volume steps
-    scale, covera = expand.volume_steps(vzero, dvolume, 11, coverazero, dcovera, 11)    #generate 11*11 steps in volume and c/a
-elif param['mod'][0] == 'simple_conv':
+    scale, covera = expand.volume_steps(vzero, dvolume, asteps, coverazero, dcovera, coasteps)    #generate 11*11 steps in volume and c/a
+elif setup['mod'] == 'simple_conv':
     covera = [coverazero]
     scale = [azero]
 else:
     covera = [1.0]
 
-param['scale'] = scale
-#param['rmt'] = [2.85]
-param['rgkmax'] = [6,7,8,9,10]
-param['ngridk'] = [8]
-param['swidth'] = [0.01]
-param['species'] = ['Be']
-param['covera'] = covera
-param['calchome'] = ["/fshome/tde/cluster/test3/"]
-param['speciespath'] = ["/appl/EXCITING/versions/hydrogen/species/"]
-param['templatepath'] = [elementshome + "templates/"]
-param['calculate'] = ['False']
-calc.CALC(param)
+setup['param']['scale'] = scale
+setup['param']['covera'] = covera
+
+calc.CALC(setup)
--- my_calcsetup.py
+++ my_calcsetup.py
@@ -1,45 +1,20 @@
-import sys
-elementshome = '/fshome/tde/git/my_calc/gen/elements/'      # define location of elements home here!
-sys.path.append(elementshome)
-import eos_calc_use_templ as calc
-import series
-
-param = {}
-usr = "tde"
-scale = []
-covera = []
-volume = []
-
-azero = 4.319                                   #lattice parameter
-da = 0.05                                   #steps in lattice parameter
-coverazero = 1.6                                #c/a ratio
-dcovera = 1.6/50                                #steps in c/a
-param['structure'] = ['hcp']
-param['mod'] = ['eos']
-
-expand = series.Series(param['structure'])      #instance of series expansion class
-
-scale = expand.latt_steps(azero, da, 11)    #generate 11 steps in lattice parameter
-
-if param['structure'][0] in ['hcp','hex'] and param['mod'][0] != 'simple_conv':
-    vzero = azero**3 * coverazero * 3**(1/2.)/2 #initial volume 
-    dvolume = vzero/50                          #volume steps
-    scale, covera = expand.volume_steps(vzero, dvolume, 11, coverazero, dcovera, 11)    #generate 11*11 steps in volume and c/a
-elif param['mod'][0] == 'simple_conv':
-    covera = [coverazero]
-    scale = [azero]
-else:
-    covera = [1.0]
-
-param['scale'] = scale
-#param['rmt'] = [2.85]
-param['rgkmax'] = [6,7,8,9,10]
-param['ngridk'] = [8]
-param['swidth'] = [0.01]
-param['species'] = ['Be']
-param['covera'] = covera
-param['calchome'] = ["/fshome/tde/cluster/test3/"]
-param['speciespath'] = ["/appl/EXCITING/versions/hydrogen/species/"]
-param['templatepath'] = [elementshome + "templates/"]
-param['calculate'] = ['False']
-calc.CALC(param)
+element={ 
+                'param': { 
+                             'scale' : { 'azero': 4.319, 'da': 0.05, 'steps': 11},
+                             'rmt': [2.85],
+                             'rgkmax': [6,7,8,9,10],
+                             'ngridk' : [8],
+                             'swidth': [0.01],
+                             'covera' :{ 
+                                          'coverazero': 1.6,  
+                                          'dcovera': 1.6/50, 
+                                          'steps': 11 } 
+                             },
+                'species': 'Be',
+                'structure':'hcp',
+                'mod': 'eos',
+                'calchome' :"/home/tom/cluster/test4/",
+                'speciespath': "/appl/EXCITING/versions/hydrogen/species/",
+                'elementshome': "/home/tom/git/ELEMENTS/elements/",
+                'templatepath' : "templates/",
+                'calculate' : 'False'}
