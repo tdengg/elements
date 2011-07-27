c295d0352fdcbf5012fe7c460a1600ead69a8170
Thomas Dengg <thomas.dengg@stud.unileoben.ac.at>
2011-07-27 11:34:29 +0200

    ------

--- eval_rmt.py
+++ eval_rmt.py
@@ -40,7 +40,7 @@ class RMT(object):
         self.basis, self.lattice, self.scale = self.get_structure()
 
         self.rmt = self.get_rmt(Z)
-        self.plot_structure()
+        #self.plot_structure()
         print self.rmt
         
     def get_rmt(self, Z):
@@ -48,7 +48,8 @@ class RMT(object):
         rmt_min = []
         m=len(self.basis)   #number of species
 
-        rmt = [[]]*m
+        rmt = [[ [] for i in range(m)] for i in range(m)]
+        
         op = Operations()
         
         #Loop all basis vectors:
@@ -60,13 +61,16 @@ class RMT(object):
                     n = len(self.basis[j])
                     for l in range(n):
                         op.vector2 = op.latt_to_cartesian(self.basis[j][l], self.lattice)
-                        if op.vector2 != op.vector1: rmt[j].append(op.vect_dist())
-                        
-        rmt_min = [min( np.array(rmt[i]) * op.ri_div_rj(Z[i],Z[i-1] )) for i in range(m)]                
+                        if op.vector2 != op.vector1: rmt[i][j].append(op.vect_dist())
+                        else: rmt[i][j].append(100)
+        print rmt
+        rmt_min = op.get_array_min(rmt)
+        #rmt_min = [min( np.array(rmt[i]) * op.ri_div_rj(Z[i],Z[i-1] )) for i in range(m)]                
 
-        factor = max(rmt_min) # scale muffin tin spheres
-        rmt_min = rmt_min/factor
-
+        #factor = max(rmt_min) # scale muffin tin spheres
+        #rmt_min = rmt_min/factor
+        #print rmt
+        
         return rmt_min
         
 
@@ -102,7 +106,7 @@ class RMT(object):
             while j<len(xb): s.append(self.rmt[i]); j+=1
             
             p = mlab.points3d(xb,yb,zb, s, scale_factor = 1)
-            p = mlab.points3d(xb+x,yb,zb, s, scale_factor = 1)
+            
             i+=1
         
         mlab.show()
@@ -111,14 +115,16 @@ class RMT(object):
 
 class Operations(object):
     """Vector operations
-    input:  -self.vector1
-            -self.vector2            
+    
+        input:  -self.vector1
+                -self.vector2            
     """
     
     def __init__(self):
         self.vector1 = []
         self.vector2 = []
-
+        self.minarray = []
+        self.index = []
     def vect_dist(self):
         """Calculate vector norm."""
         diff_vect = np.array(self.vector1) - np.array(self.vector2)
@@ -128,14 +134,17 @@ class Operations(object):
         """Calculate ratio of muffin tin radii."""
         eta = 1
         return (1. + eta*Z_j**(1./3.))/(1. + eta*Z_i**(1./3.))
+        
     def latt_to_cartesian(self,base_vect,latt):
-        """
-        Calculate cartesian basis from lattice coordinates.
-        vector1 = basis; vector2 = lattice
-        """
-        
+        """Calculate cartesian basis from lattice coordinates."""
         return list(np.dot(base_vect,[map(float,l) for l in latt]))
-        
-
+    def get_array_min(self,array):
+        """Get minimal value and index of entry in multidimensional array."""
+        #Recursively search array:
+        for i in range(len(array)):
+            if type(array[i]) == list: self.get_array_min(array[i])
+            else: self.minarray.append(min(array)); break 
+        return min(self.minarray)
+            
 if __name__=='__main__':                
     RMT('input.xml', 1, [2,10])
